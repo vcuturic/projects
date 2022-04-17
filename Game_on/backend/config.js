c15a768591715e08815ee46246e25a0779026441
Vildan Cuturic <vikky15.99@gmail.com>
2022-04-17 09:05:11 +0200

    Removed conn string

--- Game_on/backend/config.js
+++ Game_on/backend/config.js
@@ -1,7 +1,7 @@
 config = {
     port: 5000,
     // DO NOT FORGET TO CHANGE <password> and "myFirstDataBase"
-    dbConnection: "mongodb+srv://vcuturic:admin@cluster0.jednq.mongodb.net/vch_gaming?retryWrites=true&w=majority"
+    
 }
 
 module.exports = config
