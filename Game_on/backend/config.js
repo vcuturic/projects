9ed0f79aabcb47df4d025375dbb2126f6e0481bb
vcuturic <v.cuturic99@gmail.com>
2023-01-07 01:03:35 +0100

    Moving Game_On project into separate repository

--- Game_on/backend/config.js
+++
@@ -1,7 +0,0 @@
-config = {
-    port: 5000,
-    // DO NOT FORGET TO CHANGE <password> and "myFirstDataBase"
-    
-}
-
-module.exports = config
--- Game_on/backend/constants/twitchconstants.js
+++
@@ -1,5 +0,0 @@
-module.exports = Object.freeze({
-    CLIENT_ID: '***************',
-    CLIENT_SECRET: '******************',
-    GRANT_TYPE: '***************'
-});
